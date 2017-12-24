9d10ef1975da7696eb708de969654631d08ad4a9
Alyaa <the94air@gmail.com>
2017-12-24 11:22:14 +0200

    first commit

---
+++ vstack/vstack/urls.py
@@ -0,0 +1,10 @@
+"""
+vstack URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
